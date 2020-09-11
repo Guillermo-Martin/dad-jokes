bee907b37207fe0e39d774f6318ae6cb46f40bc8
gcmartin <gscalica@gmail.com>
2020-09-11 13:48:11 -0700

    Create components folder and Joke component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Joke from './components/Joke'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Dad Jokes</h1>
+      <Joke />
     </div>
   );
 }
---
+++ src/components/Joke.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Joke() {
+  return (
+    <div>
+      <h1>this is a joke</h1>
+    </div>
+  );
+}
+
+export default Joke;
