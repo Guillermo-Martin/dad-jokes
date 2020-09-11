e27a8b8a7a67213ea48cd102fad82c5f823e2b23
gcmartin <gscalica@gmail.com>
2020-09-11 15:32:07 -0700

    Add componentDidMount to make API request

--- src/components/JokeDisplay.js
+++ src/components/JokeDisplay.js
@@ -5,6 +5,17 @@ import Joke from './Joke';
 class JokeDisplay extends Component {
 
   // make an API request when JokeDisplay renders
+  async componentDidMount() {
+    // for the GET request to respond with JSON , we have to pass in "headers" with the GET request
+    try {
+      let res = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: "application/json" } }); 
+ 
+      console.log(res.data);
+
+    } catch (err) {
+      console.log(err);
+    }  
+  }
 
 
   render() {
