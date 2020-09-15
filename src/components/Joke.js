087e9d4d0ee096351b720df20357b75bc4d152fb
gcmartin <gscalica@gmail.com>
2020-09-15 15:35:26 -0700

    Create Styles folder and Joke.css

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import './../assets/styles/Joke.css';
 
 function Joke(props) {
   return (
     
-      <div>
+      <div className="Joke-line">
         <button onClick={props.increment} value={props.id}>Vote Up</button>
         <p>{props.score}</p>
         <button onClick={props.decrement} value={props.id}>Vote Down</button>
         <p>{props.text}</p>
-        <p>{props.id}</p>
       </div>
     
   );
