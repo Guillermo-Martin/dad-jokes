cbb78c4305109647e53ffb42d40e356ce356f017
gcmartin <gscalica@gmail.com>
2020-09-16 14:40:18 -0700

    Create function in Joke component to dynamically change colors

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './../assets/styles/Joke.css';
 
 function Joke(props) {
+  // create function to dynamically change colors
+  function changeColor() {
+    if(props.score >= 5) {
+      return "darkGreen";
+    } else if (props.score < 0) {
+      return "red";
+    } else {
+      return "lightGreen";
+    }
+  }
+
   return (
     
       <div className="Joke-line">
         <button onClick={props.increment} value={props.id}>Vote Up</button>
-        <p>{props.score}</p>
+        {/* color in style will call the changeColor function */}
+        <p style={ {color: changeColor()} }>{props.score}</p>
         <button onClick={props.decrement} value={props.id}>Vote Down</button>
         <p>{props.text}</p>
       </div>
